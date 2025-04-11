25ab4f14fadae9e50309a8a6d78c8fe100ef09cd
debug-droid <diegoprogbr@hotmail.com>
2025-04-10 21:03:54 -0300

    Tarefa concluída

--- TarefaLoop/src/Main.java
+++ TarefaLoop/src/Main.java
@@ -4,16 +4,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        double nota;
-        System.out.println("Digite a sua nota: ");
-        nota = sc.nextDouble();
+        double nota1;
+        double nota2;
+        double nota3;
+        double nota4;
+        double media;
 
-        if(nota >= 7) {
-            System.out.println("Aprovado!");
-        } else if(nota >= 5) {
-            System.out.println("Recuperação!");
+        System.out.println("Digite a sua primeira nota: ");
+        nota1 = sc.nextDouble();
+        System.out.println("Digite a sua segunda nota: ");
+        nota2 = sc.nextDouble();
+        System.out.println("Digite a sua terceira nota: ");
+        nota3 = sc.nextDouble();
+        System.out.println("Digite a sua quarta nota: ");
+        nota4 = sc.nextDouble();
+
+        media = ((nota1 + nota2 + nota3 + nota4) / 4);
+
+        if(media >= 7) {
+            System.out.println("Sua média é: " + media + ", você está aprovado!");
+        } else if(media >= 5) {
+            System.out.println("Sua média é: "+ media +", você está de recuperação!");
         } else {
-            System.out.println("Reprovado!");
+            System.out.println("Sua média é: "+ media + ", você está reprovado!");
         }
 
     }
